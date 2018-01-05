f1a5d54703a521ee348028ed4fcd5cb8420c2300
lastship <>
2018-01-05 20:19:47 +0100

    plugin.video.lastship-2.0.0

---
+++ service.py
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+
+"""
+    Lastship Add-on (C) 2017
+    Credits to Placenta and Covenant; our thanks go to their creators
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# Addon Name: Lastship
+# Addon id: plugin.video.lastship
+# Addon Provider: LastShip
+
+from resources.lib.modules import log_utils
+from resources.lib.modules import control
+
+control.execute('RunPlugin(plugin://%s)' % control.get_plugin_url({'action': 'service'}))
+
+try:
+    ModuleVersion = control.addon('script.module.lastship').getAddonInfo('version')
+    AddonVersion = control.addon('plugin.video.lastship').getAddonInfo('version')
+    RepoVersion = control.addon('repository.lastship').getAddonInfo('version')
+
+    log_utils.log('######################### lastship ############################', log_utils.LOGNOTICE)
+    log_utils.log('####### CURRENT lastship VERSIONS REPORT ######################', log_utils.LOGNOTICE)
+    log_utils.log('### lastship PLUGIN VERSION: %s ###' % str(AddonVersion), log_utils.LOGNOTICE)
+    log_utils.log('### lastship SCRIPT VERSION: %s ###' % str(ModuleVersion), log_utils.LOGNOTICE)
+    log_utils.log('### lastship REPOSITORY VERSION: %s ###' % str(RepoVersion), log_utils.LOGNOTICE)
+    log_utils.log('###############################################################', log_utils.LOGNOTICE)
+except:
+    log_utils.log('######################### lastship ############################', log_utils.LOGNOTICE)
+    log_utils.log('####### CURRENT lastship VERSIONS REPORT ######################', log_utils.LOGNOTICE)
+    log_utils.log('### ERROR GETTING lastship VERSIONS - NO HELP WILL BE GIVEN AS THIS IS NOT AN OFFICIAL lastship INSTALL. ###', log_utils.LOGNOTICE)
+    log_utils.log('###############################################################', log_utils.LOGNOTICE)
